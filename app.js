93294b4e28a76fb0228704cc63809e1a6f369bb7
zoombooze <zakyahmad0023@gmail.com>
2024-01-24 21:27:53 +0700

    adding a bit

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // This starts here
+
+
+
+
+
+
+/* 
+SEQUELIZE MIGRATION COMMAND
+
+npx sequelize-cli model:generate --name User --attributes username:string,password:string,ProfileId:integer
+
+npx sequelize-cli model:generate --name Profile --attributes name:string,email:string,phone:string,gender:string,birthOfDate:date
+
+npx sequelize-cli model:generate --name Book --attributes title:string,description:string,text:blob,genre:string,AuthorId:integer
+
+npx sequelize-cli model:generate --name UserReadBook --attributes UserId:integer,BookId:integer
+
+npx sequelize-cli model:generate --name Author --attributes name:string,age:integer,gender:string
+
+*/
